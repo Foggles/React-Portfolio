3a22291de15768a6c0a770a4e4b1ccfd9da7d503
Jack Fogarty <jackfogarty2105@gmail.com>
2021-04-23 13:15:11 +0800

    Temporary NavBar with working routing between root and project1 & project2

--- my-app/src/components/header.js
+++ my-app/src/components/header.js
@@ -8,12 +8,13 @@ import Button from "react-bootstrap/Button";
 export default function Header() {
     return (
         <Navbar bg="light" expand="lg">
-            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+            <Navbar.Brand href="/">Jack Fogarty Portfolio</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
-                    <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#link">Link</Nav.Link>
+                    <Nav.Link href="/">About</Nav.Link>
+                    <Nav.Link href="/project1">Project 1</Nav.Link>
+                    <Nav.Link href="/project2">Project 2</Nav.Link>
                     <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                         <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                         <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
--- my-app/src/components/project.js
+++ my-app/src/components/project.js
@@ -1,7 +1,19 @@
 import React from 'react'
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+import Header from "./Header";
 
 export default function Project(props) {
-    return(
-        <h1>{props.text}</h1>
+    return (
+        <Container>
+            <Header />
+            <Row>
+                <Col>
+                    <h1>{props.text}</h1>
+                </Col>
+            </Row>
+        </Container>
     )
 };
