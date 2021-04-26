2afa955c8e76aa510633370e1f72bd4a02d841ee
Jack Fogarty <jackfogarty2105@gmail.com>
2021-04-26 22:00:41 +0800

    Edited Project component, adding text & cards for use

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -14,25 +14,22 @@ function App() {
           <About />
         </Route>
         <Route exact path="/project1">
-          <Project text="Project1"/>
+          <Project text="Project1- A Web Developer's Guide to the Galaxy" fileName="project1"/>
         </Route>
         <Route exact path="/project2">
-          <Project text="Project2"/>
+          <Project text="Project2" fileName="project2"/>
         </Route>
         <Route exact path="/project3">
-          <Project text="Project3 - WIP"/>
+          <Project text="Project3 - WIP" fileName="project3"/>
         </Route>
         <Route exact path="/readme_generator">
-          <Project text="Readme Generator"/>
+          <Project text="Readme Generator" fileName="readmegen"/>
         </Route>
         <Route exact path="/password_generator">
-          <Project text="Password Generator"/>
+          <Project text="Password Generator" fileName="passgen"/>
         </Route>
         <Route exact path="/team_profile_generator">
-          <Project text="Team Profile Generator"/>
-        </Route>
-        <Route exact path="/">
-          <Project />
+          <Project text="Team Profile Generator" fileName="teamgen"/>
         </Route>
       </Switch>
     </BrowserRouter>
--- my-app/src/components/project.js
+++ my-app/src/components/project.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
 
 import Header from "./Header";
 import Footer from "./Footer";
@@ -12,7 +13,49 @@ export default function Project(props) {
             <Header />
             <Row className="navBarBug">
                 <Col>
-                    <h1>{props.text}</h1>
+                    <h3>{props.text}</h3>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <Card>
+                        <Card.Img variant="top" src="holder.js/100px180" />
+                        <Card.Body>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the bulk
+                                of the card's content.
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col>
+                    <h6>This is what the project is about.</h6>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <Card>
+                        <Card.Img variant="top" src="holder.js/100px180" />
+                        <Card.Body>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the bulk
+                                of the card's content.
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <Card>
+                        <Card.Img variant="top" src="holder.js/100px180" />
+                        <Card.Body>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the bulk
+                                of the card's content.
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
                 </Col>
             </Row>
             <Footer />
