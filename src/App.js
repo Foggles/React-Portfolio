fc80082de006af1061297ea06fe64d1a8c9ba061
Jack Fogarty <jackfogarty2105@gmail.com>
2021-06-09 14:21:25 +0800

    Finished all pages.

--- src/App.js
+++ src/App.js
@@ -11,7 +11,15 @@ function App() {
     <BrowserRouter>
       <Switch>
         <Route exact path="/">
-          <About />
+          <About myPicture="https://res.cloudinary.com/denkxexyj/image/upload/v1623219338/IMG_1126_-_Copy_bujotw.jpg"
+            myName="Jack Fogarty"
+            firstParagraph="G'Day, I'm Jack. I'm a nineteen year old guy from Perth, WA who is currently a student in web
+          development. I am also currently doing my Bachelor of Information Technolgoy at Murdoch University."
+            secondParagraph="I'm an avid gamer, particularly of Grand Strategy Games. I've also recently started collecting vinyl
+          records in a wide range of genres. When it comes to music I have an ecclectic music
+          taste, and enjoy music from practically any genre. However I would describe myself a metalhead,
+          since heavy metal is my favourite genre overall. You can find my current top six albums below,
+          and if you click on the album art the album will open in Spotify."/>
         </Route>
         <Route exact path="/project1">
           <Project text="Project 1- A Web Developer's Guide to the Galaxy" firstImage="https://res.cloudinary.com/denkxexyj/image/upload/v1621057825/project1_1_rwf3bi.png"
@@ -30,29 +38,29 @@ function App() {
         <Route exact path="/project3">
           <Project text="Project 3 - Varanus" firstImage="https://res.cloudinary.com/denkxexyj/image/upload/v1621240131/p3_1_caunq0.png"
             secondImage="https://res.cloudinary.com/denkxexyj/image/upload/v1621240130/p3_2_dzkjsg.png" thirdImage="https://res.cloudinary.com/denkxexyj/image/upload/v1621240131/p3_3_o3uabn.png"
-            firstImageText="" secondImageText="" thirdImageText=""
-            summaryText=""
-            repositoryLink="https://github.com/Foggles/Project-3" />
+            firstImageText="Homepage of Varanus. Displaying the games logo, along with three (albeit not great) guides." secondImageText="The character screen, where a user can create new characters, delete characters, and play as characters." thirdImageText="The combat screen, the enemy is at the top and the player's is at the bottom, where their abilities are."
+            summaryText="Project 3 - Varanus is a small web game wheere a user can create a character, and fight against a simple AI opponent."
+            deploymentLink="https://varanus-project3.herokuapp.com/" repositoryLink="https://github.com/Foggles/Project-3" />
         </Route>
         <Route exact path="/readme_generator">
           <Project text="Readme Generator" firstImage="https://res.cloudinary.com/denkxexyj/image/upload/v1621240145/RG_1_ueqi3j.png"
             secondImage="https://res.cloudinary.com/denkxexyj/image/upload/v1621240145/RG_2_o0vgsc.png" thirdImage="https://res.cloudinary.com/denkxexyj/image/upload/v1621240145/RG_3_tolhcq.png"
-            firstImageText="" secondImageText="" thirdImageText=""
+            firstImageText="Screenshot showing the complete list of questions asked by the program to the user." secondImageText="Screenshot showing the JSON data of the created README." thirdImageText="Screenshot showing the created README."
             summaryText="With this program, the user can generate a full README.md file for their project just by entering information when prompted. It also generates a license.md file for the project to display the license that it operates under."
             repositoryLink="https://github.com/Foggles/Jack-s-README-Generator" />
         </Route>
         <Route exact path="/password_generator">
           <Project text="Password Generator" firstImage="https://res.cloudinary.com/denkxexyj/image/upload/v1621240137/PG_1_ckrfnd.png"
             secondImage="https://res.cloudinary.com/denkxexyj/image/upload/v1621240137/PG_2_h8gmxq.png" thirdImage="https://res.cloudinary.com/denkxexyj/image/upload/v1621240137/PG_3_ncy5mm.png"
-            firstImageText="" secondImageText="" thirdImageText=""
-            summaryText=""
+            firstImageText="Screenshot showing the creation screen. With no options selected." secondImageText="Screenshot showing all options selected." thirdImageText="Screenshot showing a generated password with all options selected."
+            summaryText="This program can be used to create a custom password, with user customisability for what the password will be comprised of."
             deploymentLink="https://foggles.github.io/Jack-s-Password-Generator/" repositoryLink="https://github.com/Foggles/Jack-s-Password-Generator" />
         </Route>
         <Route exact path="/team_profile_generator">
           <Project text="Team Profile Generator" firstImage="https://res.cloudinary.com/denkxexyj/image/upload/v1621240153/tg_1_agkpuv.png"
             secondImage="https://res.cloudinary.com/denkxexyj/image/upload/v1621240153/tg_2_enll58.png" thirdImage="https://res.cloudinary.com/denkxexyj/image/upload/v1621240153/tg_3_l56yxh.png"
-            firstImageText="" secondImageText="" thirdImageText=""
-            summaryText=""
+            firstImageText="Screenshot showing the questions asked by the program to the user for the first employee, the Manager. As well as the JSON result." secondImageText="Screenshot showing the creation of an Engineer employee, as well as the JSON result." thirdImageText="Screenshot showing the index.html result of the program."
+            summaryText="A program a user can use to create a list of employees in their team."
             repositoryLink="https://github.com/Foggles/Jack-s-Team-Profile-Generator" />
         </Route>
       </Switch>
--- src/components/About.js
+++ src/components/About.js
@@ -2,17 +2,25 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
 
 import Header from "./Header";
 import Footer from "./Footer";
 
-export default function About() {
+export default function About(props) {
     return (
         <Container>
             <Header />
             <Row className="navBarBug">
                 <Col>
-                    <h1>Hello</h1>
+                    <Card border="dark">
+                        <Card.Img src={props.myPicture} />
+                    </Card>
+                </Col>
+                <Col>
+                    <h1>{props.myName}</h1>
+                    <p>{props.firstParagraph}</p>
+                    <p>{props.secondParagraph}</p>
                 </Col>
             </Row>
             <Footer />
